39ad7909eef7cb4989deeaae3f9a7ae56e9fcfdf
gfelicierta@gmail.com <gfelicierta@gmail.com>
2020-07-12 12:08:25 +0800

    Initial Code Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '',redirectTo:'/home',pathMatch:'full'},
+  {path : 'home', component:ContentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  
+  // ImagePath : string = 'https://s3.amazonaws.com/StartupStockPhotos/uploads/26.jpg';
+  ImagePath : string = './assets/vr.jpg';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
