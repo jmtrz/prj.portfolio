80a59859c14902df76985b23e376ce4a932bff6b
EngrJamester <gfelicierta@gmail.com>
2020-08-23 15:31:22 +0800

    Release v1

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -9,6 +9,9 @@ export class ContentComponent implements OnInit {
   
   // ImagePath : string = 'https://s3.amazonaws.com/StartupStockPhotos/uploads/26.jpg';
   ImagePath : string = './assets/vr.jpg';
+  ContactImage : string = '/assets/dev.png';
+  email : string = "/assets/email.png";
+  
   constructor() { }
 
   ngOnInit(): void {
