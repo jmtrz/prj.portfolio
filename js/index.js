ae9b99cfb50512ef70aea827c9036c6cd4edad73
Jamester Carbon Go <gfelicierta@gmail.com>
2023-02-28 14:07:27 +0800

    added responsive design and redesign

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+      const slides = button.closest("[data-carousel]").querySelectorAll("[data-slides]");
+
+      const activeSlide = slides.querySelectorAll("[data-active]");
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+      
+      if(newIndex < 0)  newIndex = slides.children.length - 1;
+
+      if(newIndex >= slides.children.length) newIndex = 0;
+
+      slides.children[newIndex].dataset.active = true;
+      delete activeSlide.dataset.active;
+      
+      
+    });
+});
+
+function onTest(){
+  console.log("hello");
+}
+
+//Mobile Nav Bar
+
+const navButton = document.querySelector('nav-button-mobile');
