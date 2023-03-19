c2489d3198970f8311c852962f12a37d0ee2fbcb
Jamester Carbon Go <gfelicierta@gmail.com>
2023-03-19 13:15:46 +0800

    added dynmic selection in skills | updated skills section | added new responsive action

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 
-
-
-//Mobile Nav Bar
-
 // Get a reference to the button element
-var button = document.querySelector('.nav-button-mobile');
+var allButton = document.querySelector('.select-all');
+var webButton = document.querySelector('.select-web');
+var mobileButton = document.querySelector('.select-mobile');
+var cicdButton = document.querySelector('.select-cicd');
+var cards = document.querySelectorAll('.skill-card');
 
+var button = document.querySelector('.nav-button-mobile');
 var navList = document.querySelector('.nav-list');
+
+var customBorderButtom = "2px solid #00C1FF";
 
 // Check if the button element exists
 if (button) {
@@ -18,3 +21,73 @@ if (button) {
     
   });
 }
+
+
+allButton.addEventListener('click', function() {
+  clearAllBorderBottom();
+  allButton.style.borderBottom = customBorderButtom;
+  cards.forEach(function(card) {    
+    card.style.display = 'flex';
+  });
+});
+
+webButton.addEventListener('click', function() {
+  clearAllBorderBottom();
+  webButton.style.borderBottom = customBorderButtom;
+
+  cards.forEach(function(card) {
+    if (card.classList.contains('web')) {
+      card.style.display = 'flex';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
+
+mobileButton.addEventListener('click', function() {
+  clearAllBorderBottom();
+  mobileButton.style.borderBottom = customBorderButtom;
+  cards.forEach(function(card) {
+    if (card.classList.contains('mobile')) {
+      card.style.display = 'flex';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
+
+cicdButton.addEventListener('click', function() {
+  clearAllBorderBottom();
+  cicdButton.style.borderBottom = "2px solid #00C1FF";
+
+  cards.forEach(function(card) {
+    if (card.classList.contains('cicd')) {
+      card.style.display = 'flex';            
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
+
+function clearAllBorderBottom() {
+  allButton.style.borderBottom = "";
+  webButton.style.borderBottom = "";
+  mobileButton.style.borderBottom = "";
+  cicdButton.style.borderBottom = "";
+}
+
+// function handleButtonClick(event) {
+//   console.log(event.target.innerText);
+//   let section = event.target.innerText;
+
+//   cards.forEach(function(card){
+//       if(card.classList.contains('web')){
+//         card.style.display = 'block';
+//       }else {
+//         card.style.display = 'none';
+//       }
+//     }
+//   );  
+// }
+
+
